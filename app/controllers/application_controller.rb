bba8ca23928b00d1ec149388b79cb16731455161
junaidalam2 <junaid.alam2@gmail.com>
2024-07-28 16:04:56 -0700

    finished bootstrap on devise and homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:position, :department, :profile])
+    end
+
+    def after_sign_in_path_for(resource)
+        root_path 
+    end
+
+    def after_sign_out_path_for(resource)
+        session.clear
+        root_path
+     end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @showed_user = User.find(params[:id])
   end
 
   # GET /users/new
