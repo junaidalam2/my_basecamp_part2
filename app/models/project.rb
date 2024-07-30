c30ad3afd65897e989831580ab4c2ec83d668f4c
junaidalam2 <junaid.alam2@gmail.com>
2024-07-30 00:33:36 -0700

    completed many to many between users and projects - forms, views, controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-    has_many :project_teams
+    has_many :project_teams, dependent: :destroy
     has_many :users, through: :project_teams
     validates :name, :description, :status, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :project_teams
+  has_many :project_teams, dependent: :destroy
   has_many :projects, through: :project_teams
   validates :first_name, :last_name, :position, :department, :profile, :email, presence: true
 
