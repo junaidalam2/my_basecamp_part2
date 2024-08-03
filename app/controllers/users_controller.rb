43a338c2a3caf59b348791e7e34ab5a59ace2b45
junaidalam2 <junaid.alam2@gmail.com>
2024-08-03 02:00:42 -0700

    updated user update controller for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,11 @@ class UsersController < ApplicationController
       if @user.update(user_params)
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
+
+        if @user.profile == 'admin'
+          session[:is_admin] = true
+        end
+
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
