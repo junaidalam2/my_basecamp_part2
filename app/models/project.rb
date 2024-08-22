61dc60a9da2c7f95e69827c33986494c0345d675
junaidalam2 <junaid.alam2@gmail.com>
2024-08-22 11:59:38 -0700

    finished project form with team lead option

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ApplicationRecord
     has_many :project_teams, dependent: :destroy
     has_many :users, through: :project_teams
-    belongs_to :team_lead, class_name: 'User'
+    belongs_to :team_lead, class_name: 'User', optional: true
     accepts_nested_attributes_for :project_teams, allow_destroy: true
     validates :name, :description, :status, presence: true
+    #validates :team_lead, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,18 @@
 #   end
 
 
+=begin
+
+user = User.create! :first_name => 'FakeFirstName1', :last_name => 'FakelastName1', :email => 'fakeaccount1@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'user', :position => 'Fake Position'
+user = User.create! :first_name => 'Junaid', :last_name => 'Alam', :email => 'junaid.alam2@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'admin', :position => "World's Best Boss"
+user = User.create! :first_name => 'FakeFirstName2', :last_name => 'FakelastName2', :email => 'fakeaccount2@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'user', :position => 'Fake Position'
+user = User.create! :first_name => 'FakeFirstName3', :last_name => 'FakelastName3', :email => 'fakeaccount3@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'user', :position => 'Fake Position'
+user = User.create! :first_name => 'FakeFirstName4', :last_name => 'FakelastName4', :email => 'fakeaccount4@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'user', :position => 'Fake Position'
+user = User.create! :first_name => 'FakeFirstName5', :last_name => 'FakelastName5', :email => 'fakeaccount5@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'user', :position => 'Fake Position'
+user = User.create! :first_name => 'FakeFirstName6', :last_name => 'FakelastName6', :email => 'fakeaccount6@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'user', :position => 'Fake Position'
+user = User.create! :first_name => 'FakeFirstName7', :last_name => 'FakelastName7', :email => 'fakeaccount7@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'user', :position => 'Fake Position'
+user = User.create! :first_name => 'FakeFirstName8', :last_name => 'FakelastName8', :email => 'fakeaccount8@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'user', :position => 'Fake Position'
+user = User.create! :first_name => 'FakeFirstName9', :last_name => 'FakelastName9', :email => 'fakeaccount9@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'user', :position => 'Fake Position'
+user = User.create! :first_name => 'FakeFirstName10', :last_name => 'FakelastName10', :email => 'fakeaccount10@gmail.com', :password => 'password1', :password_confirmation => 'password1', :department => 'Fake Deparment', :profile => 'admin', :position => 'Fake Position'
+
+=end
